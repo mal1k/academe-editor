9a3fb094382711c574636c946919b6acea2f746b
mal1k <00154@ukr.net>
2021-09-14 19:42:45 +0300

    fixed double session

--- wp-content/themes/academe/templates/partials/modals/create-session.php
+++ wp-content/themes/academe/templates/partials/modals/create-session.php
@@ -30,7 +30,8 @@
                 <div class="flex-row items-center flex-center" style="margin-top: 60px;">
                     <div class="buttons">
                         <a class="nextScreen primary-btn "style="margin: 0 10px;"><?php _e('Schedule', 'academe-theme'); ?></a>
-                        <a class="start-now primary-btn "><?php _e('Start Now', 'academe-theme'); ?></a>
+                        <a class="start-now primary-btn " style="display: none"><?php _e('Start Now', 'academe-theme'); ?></a>
+                        <a class="play-now primary-btn "><?php _e('Start Now', 'academe-theme'); ?></a>
                     </div>
                 </div>
             </div>
