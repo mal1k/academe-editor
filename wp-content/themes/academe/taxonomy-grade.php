3bce6a5993245abdc725a2c3c011e82ec4acee01
mal1k <00154@ukr.net>
2021-10-22 23:26:00 +0300

    Updated filters

--- wp-content/themes/academe/taxonomy-grade.php
+++ wp-content/themes/academe/taxonomy-grade.php
@@ -5,18 +5,18 @@ $_GET['grade'] = get_queried_object()->slug;
         <section id="bySubjectFilters" class="filters-section">
             <h1><?php echo get_queried_object()->name; ?></h1>
             <?php get_template_part('templates/partials/filter-button', 'null', [
-                'filters' => ['faculty', 'grade', 'subject', 'topic']
+                'filters' => ['subject', 'topic', 'age']
             ]); ?>
 
             
         </section>
                 
             <?php // if (is_user_logged_in() && !is_user_in_role('student')) { ?>
-                <?php if (isset($_GET['faculty']) || isset($_GET['grade']) || isset($_GET['subject']) || isset($_GET['topic'])) {
+                <?php if ( isset($_GET['grade']) || isset($_GET['subject']) || isset($_GET['topic']) || isset($_GET['age'])) {
     
-                    $filter_by = ['faculty', 'grade', 'subject', 'topic'];
+                    $filter_by = ['grade', 'subject', 'topic', 'age'];
                     $tax_query = [
-                        'relation' => 'OR',
+                        'relation' => 'AND',
                     ];
                     foreach ($filter_by as $taxonomy) {
                         if (isset($_GET[$taxonomy]) && !empty($_GET[$taxonomy])) {
